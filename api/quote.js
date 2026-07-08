be7909cce1bbc1e440ce07c6d79f51f2f5f00558
Sagar kun <sagarkonojia17@gmail.com>
2026-07-08 20:07:11 +0530

    Implement random quote functionality in handler
    Added an array of quotes and modified the handler to return a random quote.

--- api/quote.js
+++ api/quote.js
@@ -1,6 +1,23 @@
-export default function handler(req, res) {
-  res.status(200).json({
+const quotes = [
+  {
     quote: "The future depends on what you do today.",
     author: "Mahatma Gandhi"
-  });
+  },
+  {
+    quote: "Believe you can and you're halfway there.",
+    author: "Theodore Roosevelt"
+  },
+  {
+    quote: "Hard work is worthless for those who don't believe in themselves.",
+    author: "Naruto Uzumaki"
+  },
+  {
+    quote: "Power comes in response to a need, not a desire.",
+    author: "Goku"
+  }
+];
+
+export default function handler(req, res) {
+  const random = quotes[Math.floor(Math.random() * quotes.length)];
+  res.status(200).json(random);
 }
